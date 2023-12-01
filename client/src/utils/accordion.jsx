eb3ba27c9b2d655d298ff49641832637d1fd7aa5
Selina-Varshney <99686864+Selina-Varshney@users.noreply.github.com>
2023-12-01 14:56:17 +0530

    first commit

---
+++ client/src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+      "As a group of students, we understand the importance of cheap prices and provide the best solution for you.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "Both buyers and sellers can rely on our fixed rates. We will not change the price after the deal is made.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "Relax and enjoy with the best prices, best comfort and best user experience. Once a trailblazer, always a trailblazer.",
+  },
+];
+export default data;
